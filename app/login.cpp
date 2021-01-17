b53de4650fdb54373ce31cb3d1561a5956a8423f
Szafa99 <61059048+Szafa99@users.noreply.github.com>
2021-01-17 22:24:14 +0100

    Implemented login backend.
    Fixed problem with setusername method. Unit test passed successfully

---
+++ app/login.cpp
@@ -0,0 +1,90 @@
+#include "headers/login.h"
+
+
+Login::Login(Session *msession,QObject *parent):
+    QObject(parent),
+    clientid(""),
+    startsession(msession),
+    client_db(&Data_base::get_instance() ),
+    pin("pin"),
+    username("martinszafarczyk@gmail.com")
+{
+}
+
+Login::~Login()
+{
+   // delete client_db; changed database to Singelton, deleted "delte" method
+
+}
+
+
+bool Login::setpin(const QString &mpin)
+{    
+
+    if(mpin.length()==4)
+    {
+        pin=mpin;
+        emit pinChanged();
+        return true;
+    }
+    else{
+        pin=mpin;
+        emit pinChanged();
+        loginerror="Pin must have 4 digits";
+        emit loginerrorChanged();
+        return false;
+    }
+}
+
+
+bool Login::setusername(const QString &musername){
+
+    loginerror.clear();
+     username=musername;
+        emit loginerrorChanged();
+    bool contains_at,contains_dot=false;
+    for(int i =0;i<musername.length();i++)
+    {
+        if(musername[i]=='.' && contains_at)contains_dot=true;
+        if(musername[i]=='@')contains_at=true;
+    }
+    if(!contains_at || !contains_dot){
+        loginerror+="Email is incorret";
+                emit loginerrorChanged();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+
+bool Login::loguserin(const QString &mpin,const QString &musername)
+{
+    qDebug()<<"username:"<<musername<<" pin :"<<mpin;
+    if(setusername(musername) && setpin(mpin) && client_db->validet_user(musername,mpin,clientid)){
+        startsession->client_id=clientid;
+        startsession->settransfers();
+
+        return true;
+    }
+
+    else{
+        loginerror="Wrong pin or username";
+        emit loginerrorChanged();
+        return false ;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
