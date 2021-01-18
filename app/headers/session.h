72f5be8a9f4b6fc3ca6172c9bc9e44fb4fd8312c
Szafa99 <61059048+Szafa99@users.noreply.github.com>
2021-01-18 12:11:35 +0100

    added session class

---
+++ app/headers/session.h
@@ -0,0 +1,36 @@
+#ifndef SESSION_H
+#define SESSION_H
+
+#include <QObject>
+#include <QRegularExpression>
+
+#include "database.h"
+
+
+
+class Data_base;
+class Session : public QObject
+{
+    friend class Login;
+    Q_OBJECT
+
+public:
+    explicit Session(QObject *parent=nullptr);
+    Q_INVOKABLE void settransfers();
+    Q_INVOKABLE QVector<QVariantMap> gettransfers() const {return transfers ;};
+    Q_INVOKABLE QString get_data_from_clients_table(QString record)const;
+
+    static QString getclientId(){return client_id;};
+
+
+
+
+private:
+    static QString client_id;
+
+    Data_base *db;
+    QVector<QVariantMap> transfers;
+
+};
+
+#endif // SESSION_H
