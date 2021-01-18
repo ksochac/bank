fd08c670c71724d4394a08fdb3c81faac582948f
Szafa99 <61059048+Szafa99@users.noreply.github.com>
2021-01-18 12:10:35 +0100

    added session.cpp

---
+++ app/source/session.cpp
@@ -0,0 +1,19 @@
+#include "headers/session.h"
+
+Session::Session(QObject *parent) : QObject(parent),
+    db(&Data_base::get_instance())
+{}
+QString Session::client_id=0;
+
+QString Session::get_data_from_clients_table(QString record) const
+{
+    return db->getclient_data(record,Data_base::clients,client_id);
+}
+
+void Session::settransfers()
+{
+    transfers = db->set_clients_transfers(client_id);
+}
+
+
+
