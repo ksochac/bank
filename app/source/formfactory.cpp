83591c74956b336e85f0027280d1d8cae6b059ad
Szafa99 <61059048+Szafa99@users.noreply.github.com>
2021-01-21 17:27:10 +0100

    Update formfactory.cpp

--- app/source/formfactory.cpp
+++ app/source/formfactory.cpp
@@ -1,6 +1,6 @@
 #include "headers/formfactory.h"
 
-
+// creeates form objects, and exposes them to the qmlengine and appengine
 Form * FormFactory::CreateForm(QString qmlObjectName)
 {
         if(!QmlEngine)
@@ -11,13 +11,14 @@ Form * FormFactory::CreateForm(QString qmlObjectName)
             return form;
         }
 }
-
+// sets the appengine
 void FormFactory::setAppEngine(QQmlEngine *engine)
 {
     if(engine)
     appEngine=engine;
 }
 
+//sets the qmlengiine
 void FormFactory::setQmlEngine(QObject *engine)
 {
         QmlEngine=engine;
