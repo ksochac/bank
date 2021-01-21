f0d20aef1436e2fbdfac12b434eb98e40c251b0c
Szafa99 <61059048+Szafa99@users.noreply.github.com>
2021-01-21 17:23:50 +0100

    Update exchange.cpp

--- app/source/exchange.cpp
+++ app/source/exchange.cpp
@@ -1,6 +1,6 @@
 #include "headers/exchange.h"
 
-
+// sets the modeldata of the currencyform
 Exchange::Exchange(QObject *parent) : QObject(parent),
     form(FormFactory::getInstance().CreateForm("Form")),
     db(&Data_base::get_instance())
@@ -9,6 +9,7 @@ Exchange::Exchange(QObject *parent) : QObject(parent),
     form->mform_edited.insert("Result", false );
 }
 
+// returns the result of the exchange
 QString Exchange::getresult(QString amount, double converter)
 {
     QVariant res=amount.toDouble()*converter;
