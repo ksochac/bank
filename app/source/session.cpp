a70c4263072196db576be2229d182e16f40a7263
Szafa99 <61059048+Szafa99@users.noreply.github.com>
2021-01-21 17:32:45 +0100

    Update session.cpp

--- app/source/session.cpp
+++ app/source/session.cpp
@@ -5,11 +5,12 @@ Session::Session(QObject *parent) : QObject(parent),
 {}
 QString Session::client_id=0;
 
+// returns the given data the clients asks for from the clients table
 QString Session::get_data_from_clients_table(QString record) const
 {
     return db->getclient_data(record,Data_base::clients,client_id);
 }
-
+ // sets the transfers
 void Session::settransfers()
 {
     transfers = db->set_clients_transfers(client_id);
